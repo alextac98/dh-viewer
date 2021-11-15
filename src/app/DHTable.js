43a8e5e5d38ce44ecedd8a61a2095eb287a4be9b
Alex Tacescu <alextac98@gmail.com>
2021-11-15 07:21:20 +0000

    Add simple app structure with table and visualizer divs

---
+++ src/app/DHTable.js
@@ -0,0 +1,10 @@
+import './App.css'
+// Look to use table html tag: https://developer.mozilla.org/en-US/docs/Web/HTML/Element#table_content
+
+export function DHTable(){
+    return (
+        <div className="dhtable">
+
+        </div>
+    );
+}
